55d58d172352d50038c1c3483f54246bd615d931
Evolusin <98921577+Evolusin@users.noreply.github.com>
2022-10-07 19:32:05 +0200

    Fixed Settings config initalize

--- bait_analize.py
+++ bait_analize.py
@@ -2,21 +2,25 @@ from windowcapture import get_screenshot, record_screen
 from settings import Settings
 from vision import Vision
 
-def get_bait_localization(monitor):
-    points = None
-    # get an updated image of the game
-    screenshot = get_screenshot(monitor)
+class BaitAnalizator():
+    def __init__(self):
+        config = Settings()
 
-    # display the processed image
-    for f in config.temp_names:
-        vision_limestone = Vision(f'{config.img_dir}{f}')
-        points = vision_limestone.find(screenshot, 0.6, 'rectangles')
-        if points:
-            print(f"Detected bait at - {points}")
-            return points
+    def get_bait_localization(self, monitor):
+        points = None
+        # get an updated image of the game
+        screenshot = get_screenshot(monitor)
 
-def analize_bait(position):
-    img_bait = get_screenshot(position)
-    recorded_bait = record_screen(img_bait)
+        # display the processed image
+        for f in config.temp_names:
+            vision_limestone = Vision(f'{config.img_dir}{f}')
+            points = vision_limestone.find(screenshot, 0.6, 'rectangles')
+            if points:
+                print(f"Detected bait at - {points}")
+                return points
+
+    def analize_bait(self, position):
+        img_bait = get_screenshot(position)
+        recorded_bait = record_screen(img_bait)
 
 config = Settings()
--- settings.py
+++ settings.py
@@ -11,7 +11,7 @@ class Settings():
         self.monitor = {"top": 0, "left": 0, "width": 2560, "height": 1440}
         self.mode = 0
         self.new_monitor = {}
-        
+        self.get_templates()
     def get_templates(self):
         for f in os.listdir(self.img_dir):
             self.temp_names.append(f)
